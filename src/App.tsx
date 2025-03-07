a3ee8cb0e65af51623e7f20a54ff9c1f6b0a75b3
edward <edwardlfish@gmail.com>
2025-03-07 09:10:56 +0200

    useNavigate in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import './App.css'
 import Guest from "./components/Guest";
 import Profile from "./components/Profile";
+import {Route, Routes, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 function App() {
+    const navigate = useNavigate();
     const token = '';
 
+    useEffect(() => {
+        if (token) {
+            navigate('/profile');
+        } else {
+            navigate('/');
+        }
+    }, [token, navigate]);
+
     return (
-        <Guest/>
-    )
+        <Routes>
+            <Route path="/" element={<Guest />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
+    );
 }
 
 export default App
